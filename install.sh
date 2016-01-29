c9e0b3720eeb8b295ab892601c08679374cc5dd7
dvd <lo.re.davide@gmail.com>
2016-01-29 15:32:33 -0500

    FIX rbenv init

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ set -e
 set -u
 set -x
 
+ruby_ver='2.2.2'
+
 # packages you'll need (plus some useful stuff)
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -y zsh git openssl libqt4-dev \
@@ -26,20 +28,25 @@ cd
 [[ -d ~/.rbenv ]] || git clone 'https://github.com/sstephenson/rbenv.git' ~/.rbenv
 echo '# sunlight' | tee -a ~/.zshrc ~/.bashrc
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' | tee -a ~/.zshrc ~/.bashrc
-echo 'eval "$(rbenv init -)"' | tee -a ~/.zshrc ~/.bashrc
+rbenv init - | tee -a ~/.zshrc ~/.bash_profile
 [[ -d ~/.rbenv/plugins/ruby-build ]] || git clone 'https://github.com/sstephenson/ruby-build.git' ~/.rbenv/plugins/ruby-build
 source ~/.bashrc
+source ~/.bash_profile
+
 # install ruby
-rbenv install 2.2.2
-rbenv rehash
-rbenv global 2.2.2
-rbenv shell 2.2.2
-rbenv rehash
+if [[ "$(rbenv version-name)" != "$ruby_ver" ]]; then
+	rbenv install "$ruby_ver"
+	rbenv rehash
+	rbenv global "$ruby_ver"
+	rbenv shell "$ruby_ver"
+	rbenv rehash
+fi
 
 #install Cassandra
 
 # clone Sunlight
-git clone 'https://github.com/columbia/sunlight.git'
+[[ -d sunlight ]] || git clone 'https://github.com/columbia/sunlight.git'
+cd sunlight
 gem install bundle
 rbenv rehash
 # install gems
